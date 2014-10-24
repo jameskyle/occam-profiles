2fd938d06fc68e951d6eba1f41fa5588c74afe63
James Kyle <james@jameskyle.org>
2014-10-24 16:18:28 -0700

    Set puppet service name to httpd for puppetdb

--- manifests/puppet/puppetdb.pp
+++ manifests/puppet/puppetdb.pp
@@ -4,7 +4,9 @@
 class profile::puppet::puppetdb {
   class { '::puppetdb':
     ssl_listen_address => '0.0.0.0',
-    java_args          => { '-Xmx' => '4g' }
+    java_args          => { '-Xmx' => '4g' },
   }
-  class{ '::puppetdb::master::config': }
+  class{ '::puppetdb::master::config': 
+    puppet_service_name => 'httpd',
+  }
 }
